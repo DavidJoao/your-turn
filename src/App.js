439bf96a4c108cb2d6ed3a7d1cbef39fc7afc868
DavidJoao <davidsandoval596@gmail.com>
2022-09-10 12:52:57 -0700

    start page done - first version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import StartPage from './components/StartPage';
 
 function App() {
   return (
     <div>
+      <StartPage />
     </div>
   );
 }
---
+++ src/components/StartPage.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Offcanvas, Button } from 'react-bootstrap'
+
+function StartPage(){
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return(
+        <div style={{ width:'100%', height:'100vh', backgroundColor:'#b8a161', display:"flex", flexDirection:'column', justifyContent:'center', alignItems:'center'}}> 
+            <h1 style={{marginBottom:"2%"}}>Your Turn: Drinking Game</h1>
+            <Button style={{border:"2px solid #524b37", backgroundColor:"transparent", boxShadow:"0px 0px 17px 0px rgba(0,0,0,0.75)", color:"black", fontWeight:"500"}} onClick={handleShow}>Don't know the rules? Click here!</Button>
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                Some text as placeholder. In real life you can have the elements you
+                have chosen. Like, text, images, lists, etc.
+                </Offcanvas.Body>
+            </Offcanvas>
+            <Button style={{border:"2px solid #524b37", backgroundColor:"transparent", margin:'1%', boxShadow:"0px 0px 17px 0px rgba(0,0,0,0.75)", color:"black"}}> Start Game</Button>
+        </div>
+    )
+}
+
+export default StartPage
