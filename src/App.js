3a2316d63e1e71aee34bb111705f366e57a4cdfc
DavidJoao <davidsandoval596@gmail.com>
2022-09-11 20:04:19 -0700

    changed style of name cards, and added overflow scroll to users list

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
 
   const colorTemplate = {
     primary: '#b8a161',
-    secondary:'#524b37'
+    secondary:'#524b37',
+    shadow: '0px 5px 19px 0px rgba(0,0,0,0.75)'
   }
 
   return (
--- src/components/Play.js
+++ src/components/Play.js
@@ -30,15 +30,15 @@ function Play(){
         <div style={{width:"100%", height:'100vh', backgroundColor:`${colorTemplate.primary}`, display:"flex", justifyContent:"center", alignItems:"center"}}>
             <div style={{border:`5px solid ${colorTemplate.secondary}`, width:'90%', height:"90vh", borderRadius:'10px', display:'flex', flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
                 <h3>Enter Players:</h3>
-                <InputGroup style={{width:'40%'}}>
+                <InputGroup style={{width:'90%'}}>
                     <Form.Control onChange={handleChange} placeholder="Player's name" value={player}/>
-                    <Button style={{backgroundColor:`${colorTemplate.primary}`, border:`1px solid ${colorTemplate.secondary}`, color:'black'}} onClick={addPlayer}>Add Player</Button>
+                    <Button style={{backgroundColor:`${colorTemplate.secondary}`, border:`1px solid ${colorTemplate.secondary}`, color:'white'}} onClick={addPlayer}>Add Player</Button>
                 </InputGroup>
-                <div style={{border:"1px solid black", width:'40%', height:"30vh", marginTop:"2%"}}>
-                    <ul>
+                <div style={{border:`2px solid ${colorTemplate.secondary}`, width:'90%', height:"30vh", marginTop:"2%", overflow:'scroll', borderRadius:"10px", boxShadow:`${colorTemplate.shadow}`}}>
+                    <ul style={{display:'flex', flexDirection:'column', alignItems:"center", padding:"1%"}}>
                         {playersList.map(player => {
                             return(
-                                <li key={player.id}>{player.value}</li>
+                                <li key={player.id} style={{padding:'1%', border:'2px solid black', borderRadius:'10px', listStyle:'none', width:'50%', textAlign:"center", margin:"1%", backgroundColor:`white`, boxShadow:"0px 5px 19px 0px rgba(0,0,0,0.75)", fontSize:"1rem"}}>{player.value}</li>
                             )
                         })}
                     </ul>
